a345262e16c2dc212d928436b2473eb27ea70332
tiandiyun <464780187@qq.com>
2020-08-24 02:46:48 +0800

    发送http信息头部调整

--- GeneralTools/src/Tool/HttpInterface.py
+++ GeneralTools/src/Tool/HttpInterface.py
@@ -11,6 +11,7 @@ def GetPhpAgent():
 	header = {}
 	header['p_pradar_traceid'] = '112211'
 	header['Proxy-Client-IP'] = '10.1.3.47'
+	header['X-Forwarded-For'] = '192.168.1.5'
 	return requests.get('http://localhost:8900/work/phptest/main.php', headers=header)
 
 
